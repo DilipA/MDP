c5aabfd2c26cdb5ea55b9e96f68c13399b938b2a
Dilip <dilip.arumugam@gmail.com>
2017-02-07 11:08:57 -0500

    Shell scripts for Grid update

---
+++ scripts/update_jar.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp ../out/artifacts/MDP_jar/MDP.jar darumuga@ssh.cs.brown.edu:MDP.jar
+
---
+++ scripts/update_plot_figure3.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scp ../src/main/python/plot/plot_figure3.py darumuga@ssh.cs.brown.edu:plot_figure3.py
